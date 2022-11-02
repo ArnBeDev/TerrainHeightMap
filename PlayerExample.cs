2fd4e0275e4dcf0118dd18b34bc283a8e1f79d4f
ArnBeDev <bethge.arnd@gmail.com>
2022-11-02 16:39:09 +0100

    Fixes #1, fixes #2, fixes #3; Added TerrainHeightMap and PlayerExample class. Added Instruction.

---
+++ PlayerExample.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerExample : MonoBehaviour
+{
+    // A simple class to demonstrate the TerrainHeightMap
+    // The Gameobject will be moved to the targetPosition  using the TerrainHeightMap
+
+    TerrainHeightMap terrain;
+     
+    public Vector3 targetPosition;
+
+    void Start()
+    {
+
+        terrain = FindObjectOfType<TerrainHeightMap>();
+
+        //gets the correct height of the targetPosition on the terrain
+        targetPosition = terrain.GetPosition(targetPosition);
+
+
+        //gets the correct height of this gameobject on the terrain
+        this.transform.position = terrain.GetPosition(this.transform.position);
+    }
+
+    void Update()
+    {
+
+        Move();
+    }
+
+
+    private void Move()
+    {
+
+        Vector3 nextPosition = Vector3.MoveTowards(this.transform.position, targetPosition, Time.deltaTime * 5);
+
+
+        // Note that we have to turn the sign of the x-coordinate
+        float height = terrain.GetHeightOfPosition(new Vector3(nextPosition.x * (-1f), 0, nextPosition.z));
+
+
+        // Move only if the nextPosition has a stored height
+        if (height > 0)
+        {
+            this.transform.position = new Vector3(nextPosition.x, height, nextPosition.z);
+        }
+
+    }
+
+
+}
